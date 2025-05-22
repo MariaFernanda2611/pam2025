e389bd1dc7c4cd8a755bd6bd47459cd3936e3e60
MariaFernanda2611 <mariafernandabritodeoliveira@gmail.com>
2025-05-22 09:55:38 -0300

    Estrutura básica

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+
+export default function Index() {
+  const nome = "Maria";
+
+  return <View>
+    <Text>Jogo da Velha</Text>
+    <Text>Vencedor</Text>
+
+    <View>
+        <View>
+            <TouchableOpacity>
+                <Text></Text>
+            </TouchableOpacity>
+        </View>
+
+        <View>
+            <TouchableOpacity>
+                <Text></Text>
+            </TouchableOpacity>
+            </View>
+
+        <View>
+        <TouchableOpacity>
+                <Text></Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+
+  </View>
+}
