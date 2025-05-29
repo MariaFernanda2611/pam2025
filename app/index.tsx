91f7299c0182d590522e79690b69c293a203494e
MariaFernanda2611 <mariafernandabritodeoliveira@gmail.com>
2025-05-29 09:58:37 -0300

    estilizado

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,110 @@
-import { Text, TouchableOpacity, View } from "react-native";
-
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const nome = "Maria";
-
-  return <View>
-    <Text>Jogo da Velha</Text>
-    <Text>Vencedor</Text>
-
-    <View>
+    const cell= () =>{
+        return(
         <View>
-            <TouchableOpacity>
-                <Text></Text>
-            </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>X</Text>
+          </TouchableOpacity>
+        </View>
+        )
+    }
+  return (
+    <View style={style.container}
+    >
+      <Text>"Jogo da Velha</Text>
+      <Text>Vencedor</Text>
+
+      <View>
+        <View>
+            {cell()}
+            {cell()}
+            {cell()}
+        </View>
         </View>
 
         <View>
-            <TouchableOpacity>
-                <Text></Text>
-            </TouchableOpacity>
+        <View>
+            {cell()}
+            {cell()}
+            {cell()}
+        </View>
+        </View>
+
+        <View>
+        <View>
+            {cell()}
+            {cell()}
+            {cell()}
+        </View>
+        </View>
+
+        <View>
+        <View>
+            {cell()}
+            {cell()}
+            {cell()}
             </View>
+        </View>
 
         <View>
-        <TouchableOpacity>
-                <Text></Text>
-            </TouchableOpacity>
+        <View>
+            {cell()}
+            {cell()}
+            {cell()}
         </View>
-    </View>
+        </View>
 
-  </View>
+        <View>
+            {cell()}
+            {cell()}
+            {cell()}
+        </View>
+
+    </View>
+  )
 }
+
+const style = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#A8C686",
+
+    },
+
+    title: {
+        fontSize:24,
+        fontWeight:"bold",
+        marginBottom:20,
+    },
+
+    status:{
+        fontSize:18,
+        marginBottom:20,
+    },
+    
+    board:{
+        marginBottom:20,
+    },
+    row:{
+        flexDirection:"row",
+    },
+    cell:{
+        width:80,
+        height:80,
+        borderWidth:2,
+        borderColor:"#",
+        alignItems:"center",
+        justifyContent:"center",
+        backgroundColor:"",
+    },
+    cellContent:{
+        fontSize:36,
+        fontWeight:"bold",
+    },
+    
+})
